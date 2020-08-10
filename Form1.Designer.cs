8fb4bb4176b338abbcb5c609a895192be4d3d025
Tom Borton <tom@retrieversolutionsinc.com>
2020-08-10 19:05:33 -0400

    changed font color on lable7

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -246,6 +246,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(40)))), ((int)(((byte)(252)))));
             this.label7.Location = new System.Drawing.Point(3, 362);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(0, 24);
